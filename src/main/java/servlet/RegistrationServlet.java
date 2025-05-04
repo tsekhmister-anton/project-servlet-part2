05b81004605edd1652b89de024ed04269c1218df
tsekhmister-anton <tsekhmister.anton@gmail.com>
2025-05-04 11:25:48 +0300

    Создал страницу registration.html. Создал style.css.

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -1,4 +1,18 @@
 package servlet;
 
-public class RegistrationServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/registration")
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
