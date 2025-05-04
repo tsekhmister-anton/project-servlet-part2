2ad637550655a2c194ab0590a367ca5ae431d24a
tsekhmister-anton <tsekhmister.anton@gmail.com>
2025-05-04 12:23:45 +0300

    Подключил style.css. Сделал перенаправление req, res в RegistrationServlet.

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -13,6 +13,6 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        req.getRequestDispatcher("/registration.html").forward(req, resp);
     }
 }
